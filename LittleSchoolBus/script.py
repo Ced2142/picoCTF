80efbb226d6a9f5b89709e37603c617c1f4243a8
Cedric Brisebois <ced2142b@gmail.com>
2019-04-03 19:54:12 -0400

    Editied comments

--- LittleSchoolBus/script.py
+++ LittleSchoolBus/script.py
@@ -7,18 +7,19 @@ height = ima.height
 binary=[]
 char=[]
 
+
 for x in range(0, height):
     for y in range(0, width):
         pixel=im[y,x]
-        for z in range(3)[::-1]:  #loop over RGB (Sometimes you need to invert because people are sneaky ([::-1]))
-            binary.append(bin(pixel[z])[-1]) #add last bit value of every red,green and blue value from the target pixles to binary array
+        for z in range(3)[::-1]:
+            binary.append(bin(pixel[z])[-1]) #add last bit value of every red,green and blue value
 
 
-for a in range(0,len(binary)-8,8):  #Groupe all bits into bytes
+for a in range(0,len(binary)-8,8):
     temp=[]
     for b in range(8):
         temp.append(binary[b+a])
-    char.append(chr(int(''.join(temp),2))) #append all ascii values from corresponding binary values to char array
+    char.append(chr(int(''.join(temp),2)))
 
 
-print(''.join(char)) #print the values
+print(''.join(char))
